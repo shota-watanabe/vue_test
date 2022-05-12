33ce4d0e23628e1f7e2193075a80ec755a9af285
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-13 06:51:53 +0900

    install Vuex

---
+++ vuex/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    // ファイルを分割するときに使う
+    modules: {}
+})
