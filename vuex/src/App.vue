670f26134e0018f3a5da8fe4d209bda77221d82d
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-13 08:21:51 +0900

    getters

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -6,5 +6,28 @@
     </nav>
     <router-view/>
     {{ $store.state.count }}
+    <br>
+    <ul>
+      <li v-for="user in visibleUsers" :key="user.id">
+      {{ user.id }} : {{ user.name }} : {{ user.isVisible }}
+      </li>
+    </ul>
+    <br>
+    {{ getUserById.name }}
   </div>
 </template>
+
+<script>
+export default {
+  computed:{
+    visibleUsers(){
+      return this.$store.getters.visibleUsers
+    },
+    getUserById(){
+      // id2のユーザーを表示
+      return this.$store.getters.getUserById(2)
+    }
+  }
+}
+</script>
+
--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -6,9 +6,26 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         // 初期値
-        count: 0
+        count: 0,
+        users: [
+            { id: 1, name: '大谷', isVisible: true },
+            { id: 2, name: 'ダルビッシュ', isVisible: false },
+            { id: 3, name: '錦織', isVisible: true },
+        ]
     },
-    getters: {},
+    getters: {
+        /* visibleUsers(state) {
+            // isVisibleがtrueなら表示
+            return state.users.filter(user => user.isVisible)
+        } */
+        // アロー関数での書き方(1行で書く場合はreturnが不要)
+        visibleUsers: state => state.users.filter(user => user.isVisible),
+        // メソッド形式
+        getUserById: state => id => {
+            // user.idがコンポーネントのidと同じなら表示
+            return state.users.find(user => user.id === id)
+        }
+    },
     // stateの値を変えるためにmutations
     mutations: {
         increment(state) {
