47a72750df0a95660778cafb213adb3d801dfa1c
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-14 18:20:56 +0900

    test

---
+++ section9/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="incrementAction">+</button>
+    <button @click="addCount">10</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods:{
+    /* increment(){
+      // mutationsの場合はcommitでmutations呼び出し
+      this.$store.commit('increment')
+    },
+
+    addCount(){
+      this.$store.commit('addCount', {
+        value: 10
+      })
+    } */
+    // 配列で複数指定可
+    ...mapActions(['incrementAction', 'addCountAction']),
+
+    /* increment(){
+      // actionはdispatch
+      this.$store.dispatch('incrementAction')
+    }, */
+    addCount(){
+      this.addCountAction({
+        // mapActionsは引数を指定できないので、他の関数から呼び出す。addCountActionを呼び出してここで引数を指定する
+        // payloadに10が入る
+        value: 10
+      })
+    }
+  }
+}
+</script>
