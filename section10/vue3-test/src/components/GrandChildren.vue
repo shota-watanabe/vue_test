2eb2c4e82623fa1144c932ef5681c6d03be91b14
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-02-28 22:03:36 +0900

    add: children view 追加

---
+++ section10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>GrandChildren(孫)</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ section10/vue3-test/src/views/Children.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Children（子）</div>
+  <Grand-Children />
+</template>
+
+<script>
+import GrandChildren from '@/components/GrandChildren'
+export default {
+  components: {
+    GrandChildren
+  }
+}
+</script>
+
+<style>
+</style>
