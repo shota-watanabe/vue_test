36d70711c780a5bb69bbe0a0e36e2b4f78ec21c5
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-01 23:57:46 +0900

    add: setup関数の実行タイミング確認

---
+++ section10/vue3-test/src/views/CompositionTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>CompositionTest</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  // 速さ順左から setup->created->mounted
+  setup() {
+    console.log('setup')
+  },
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  }
+}
+</script>
+
+<style>
+
+</style>
