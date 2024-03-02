dbcbe5efdd7d2fa27854c6e9d9698aea1d4b92f0
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-02 22:11:29 +0900

    add: ref, reactive, toRefs

--- section10/vue3-test/src/views/CompositionTest.vue
+++ section10/vue3-test/src/views/CompositionTest.vue
@@ -3,36 +3,56 @@
     CompositionTest
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <p>ref: {{ nameRef }}</p>
+    <p>reactive: {{ book.title }}</p>
+    <p>reactive: {{ book.author[0] }}</p>
+    <p>reactiveToRefs: {{ titleRef }}</p>
+    <p>reactiveToRefs: {{ authorRef[1] }}</p>
   </div>
 </template>
 
 <script>
+import { ref, reactive, toRefs } from "vue";
 export default {
   // 速さ順左から setup->created->mounted
   // setupではthisが使えない
   setup() {
-    let name = '大谷'
-    let age = 30
-    console.log('setup')
+    let name = "大谷";
+    const age = 30;
+    const nameRef = ref("錦織");
+    const book = reactive({
+      title: 'タイトル',
+      author: ['大谷', '伊藤']
+    })
+    const bookToRefs = reactive({
+      titleRef: 'タイトル2',
+      authorRef: ['大谷2', '伊藤2']
+    })
+
+    console.log("setup");
+    console.log(nameRef.value);
     return {
-      name, age
-    }
+      name,
+      age,
+      nameRef,
+      book,
+      // リアクティブなオブジェクトを展開するときはtoRefs
+      ...toRefs(bookToRefs)
+    };
   },
   data() {
     return {
       number: 1,
-      sports: 'サッカー'
-    }
+      sports: "サッカー",
+    };
   },
   created() {
-    console.log('created')
+    console.log("created");
   },
   mounted() {
-    console.log('mounted')
-  }
-}
+    console.log("mounted");
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
