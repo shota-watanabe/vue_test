dd1884887bd647f817bb589f893951296cd5f63c
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-02 18:01:54 +0900

    add: setup関数の戻り値の確認

--- section10/vue3-test/src/views/CompositionTest.vue
+++ section10/vue3-test/src/views/CompositionTest.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>CompositionTest</div>
+  <div>
+    CompositionTest
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {}
-  },
   // 速さ順左から setup->created->mounted
+  // setupではthisが使えない
   setup() {
+    let name = '大谷'
+    let age = 30
     console.log('setup')
+    return {
+      name, age
+    }
+  },
+  data() {
+    return {
+      number: 1,
+      sports: 'サッカー'
+    }
   },
   created() {
     console.log('created')
