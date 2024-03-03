b342ccc5922b2c6cac05a8e121800ec0f3b12648
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-03 21:04:08 +0900

    add: script内でvue-router

--- section10/vue3-test/src/App.vue
+++ section10/vue3-test/src/App.vue
@@ -7,6 +7,7 @@
     <router-link to="/composition-test">Composition</router-link> |
     <router-link to="/props-emit-test">PropsEmitTest</router-link> |
     <router-link to="/function-test">FunctionTest</router-link> |
+    <router-link to="/router-test">RouterTest</router-link> |
     <router-view
       :setupBooks="setupBooks"
       :dataBooks="dataBooks"
---
+++ section10/vue3-test/src/views/RouterTest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>RouterTest</p>
+    <button @click="goHome">ホームに戻る</button>
+    <button @click="checkRoutePath">ルート情報</button>
+  </div>
+</template>
+
+<script>
+import { useRouter, useRoute } from 'vue-router'
+
+export default {
+  setup() {
+    const router = useRouter()
+    const route = useRoute()
+
+    const goHome = () => {
+      router.push('/')
+    }
+
+    const checkRoutePath = () => {
+      console.log(route.path)
+    }
+
+    return {
+      goHome, checkRoutePath
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
