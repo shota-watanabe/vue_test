15d9a32b10041ba9ad94cb02efd4731353409119
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-03 21:26:39 +0900

    add: script内でvuex

--- section10/vue3-test/src/App.vue
+++ section10/vue3-test/src/App.vue
@@ -8,6 +8,7 @@
     <router-link to="/props-emit-test">PropsEmitTest</router-link> |
     <router-link to="/function-test">FunctionTest</router-link> |
     <router-link to="/router-test">RouterTest</router-link> |
+    <router-link to="/vuex-test">VuexTest</router-link> |
     <router-view
       :setupBooks="setupBooks"
       :dataBooks="dataBooks"
---
+++ section10/vue3-test/src/views/VuexTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>VuexTest</p>
+    <p>{{ count }}</p>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    const count = computed(() => {
+      return store.state.count
+    })
+
+    const increment = () => {
+      store.commit('increment')
+    }
+
+    return { count, increment }
+  }
+}
+</script>
+
+<style>
+
+</style>
