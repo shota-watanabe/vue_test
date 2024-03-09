292eecbe9ac4ce1f3f63422fbf7445d200a7061d
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-03-09 21:36:21 +0900

    add: script setup

--- section11/script_setup_test/src/App.vue
+++ section11/script_setup_test/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ScriptTest />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ScriptTest from './components/ScriptSetupTest'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ScriptTest
   }
 }
 </script>
